b6949aee0445cdfa301e8f6233368a88b1c84d6f
TheKCui0327 <59521762+KCui0327@users.noreply.github.com>
2022-07-14 01:02:56 -0400

    Created Program class

--- src/Program.java
+++ src/Program.java
@@ -1,5 +1,19 @@
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.border.Border;
+import javax.swing.*;
+import java.awt.BorderLayout;
+import java.awt.Container;
+import java.awt.Dimension;
+
 public class Program {
     public static void main(String args[]) {
-        
+        JFrame frame = new JFrame("QuickTest");
+        JLabel emptyLabel = new JLabel("")
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.getContentPane().add(emptyLabel, BorderLayout.CENTER);
+        frame.pack();
+        frame.setVisible(true);
     }
+
 }
