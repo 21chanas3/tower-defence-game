42444a5f7671c90bf8754217eaa1a74580ee7664
TheKCui0327 <59521762+KCui0327@users.noreply.github.com>
2022-07-18 14:54:32 -0400

    frameLayout

--- Frame.java
+++ Frame.java
@@ -3,6 +3,7 @@ import javax.swing.JPanel;
 import javax.swing.JButton;
 import javax.swing.JLabel;
 import java.awt.GridLayout;
+import assets.Cell;
 
 public class Frame extends JFrame {
     private JPanel panel;
@@ -16,5 +17,13 @@ public class Frame extends JFrame {
         setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
         setSize(500, 500);
         setVisible(true);
+        panel = new JPanel(new GridLayout(100, 100, 0 , 0));
+        for (int i = 0; i < 100; i++) {
+            for (int j = 0; j < 100; j++) {
+                Cell addCells = new Cell();
+                panel.add(addCells);
+            }
+        }
+        add(panel);
     }
 }
