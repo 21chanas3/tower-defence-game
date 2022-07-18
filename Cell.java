8812c872de521c4dd90de83d1cbb4d8172dcf925
TheKCui0327 <59521762+KCui0327@users.noreply.github.com>
2022-07-18 14:14:03 -0400

    frameLayout

---
+++ Cell.java
@@ -0,0 +1,34 @@
+import utils.Vector2D;
+
+public class Cell {
+    private int colour, width, height;
+    private Vector2D position;
+
+    public void getColour(int colour) {
+        this.colour = colour;
+    }
+
+    public int setColour() {
+        return colour;
+    }
+
+    public void getWidth(int width) {
+        this.width = width;
+    }
+
+    public int setWidth() {
+        return width;
+    }
+
+    public void getHeight(int height) {
+        this.height = height;
+    }
+
+    public int setHeight() {
+        return height;
+    }
+
+    public 
+
+
+}
